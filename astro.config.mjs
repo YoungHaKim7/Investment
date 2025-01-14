b5952e9aa90768ffef09f5bdd78cca0194163c9a
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2025-01-15 02:00:06 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://younghakim7.github.io/Investment/',
 	integrations: [mdx(), sitemap()],
 });
